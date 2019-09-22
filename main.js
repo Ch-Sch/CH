014987d82c9a88dfff246c60543957978504ea1f
Ch-Sch <54683753+Ch-Sch@users.noreply.github.com>
2019-09-23 00:15:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,19 +14,19 @@ disconnectButton.addEventListener('click', function() {
 });
 
 // Sendeformular
-sendForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Предотвратить отправку формы
-  send(inputField.value); // Отправить содержимое текстового поля
-  inputField.value = '';  // Обнулить текстовое поле
-  inputField.focus();     // Вернуть фокус на текстовое поле
-});
+//sendForm.addEventListener('submit', function(event) {
+ // event.preventDefault(); // Предотвратить отправку формы
+  //send(inputField.value); // Отправить содержимое текстового поля
+  //inputField.value = '';  // Обнулить текстовое поле
+  //inputField.focus();     // Вернуть фокус на текстовое поле
+//});
     
-let deviceCache = null;
+//let deviceCache = null;
 
-let characteristicCache = null;
+//let characteristicCache = null;
 
 // Zwischenpuffer
-let readBuffer = '';
+//let readBuffer = '';
 
 function connect() {
   return (deviceCache ? Promise.resolve(deviceCache) :
@@ -38,19 +38,34 @@ function connect() {
 
 function requestBluetoothDevice() {
   log('Requesting bluetooth device...');
+  
+  
+ navigator.bluetooth.requestDevice({ filters: [{ services: [0x2220] }] })
+.then(device => device.gatt.connect())
+.then(server => server.getPrimaryService(0x2220))
+.then(service => service.getCharacteristic(0x2221'))
+.then(characteristic => characteristic.getDescriptor('gatt.characteristic_user_description'))
+.then(descriptor => descriptor.readValue())
+.then(value => {
+  let decoder = new TextDecoder('utf-8');
+  console.log('User Description: ' + decoder.decode(value));
+})
+.catch(error => { console.log(error); });
+  
+  
 
-  return navigator.bluetooth.requestDevice({
-    filters: [{services: [0x2220]}],
-  }).
-      then(device => {
-        log('"' + device.name + '" bluetooth device selected');
-        deviceCache = device;
-        deviceCache.addEventListener('gattserverdisconnected',
-            handleDisconnection);
+//  return navigator.bluetooth.requestDevice({
+//    filters: [{services: [0x2220]}],
+//  }).
+//      then(device => {
+//        log('"' + device.name + '" bluetooth device selected');
+//        deviceCache = device;
+//        deviceCache.addEventListener('gattserverdisconnected',
+//            handleDisconnection);
 
-        return deviceCache;
-      });
-}
+//        return deviceCache;
+//      });
+//}
 
 function handleDisconnection(event) {
   let device = event.target;
@@ -58,53 +73,53 @@ function handleDisconnection(event) {
   log('"' + device.name +
       '" bluetooth device disconnected, trying to reconnect...');
 
-  connectDeviceAndCacheCharacteristic(device).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => log(error));
-}
+//  connectDeviceAndCacheCharacteristic(device).
+  //    then(characteristic => startNotifications(characteristic)).
+    //  catch(error => log(error));
+//}
 
-function connectDeviceAndCacheCharacteristic(device) {
-  if (device.gatt.connected && characteristicCache) {
-    return Promise.resolve(characteristicCache);
-  }
+//function connectDeviceAndCacheCharacteristic(device) {
+  //if (device.gatt.connected && characteristicCache) {
+    //return Promise.resolve(characteristicCache);
+  //}
 
-  log('Connecting to GATT server...');
+  //log('Connecting to GATT server...');
 
-  return device.gatt.connect().
-      then(server => {
-        log('GATT server connected, getting service...');
+  //return device.gatt.connect().
+    //  then(server => {
+      //  log('GATT server connected, getting service...');
 
-        return server.getPrimaryService(0x2220);
-      }).
-      then(service => {
-        log('Service found, getting characteristic...');
+        //return server.getPrimaryService(0x2220);
+      //}).
+      //then(service => {
+        //log('Service found, getting characteristic...');
 
-        return service.getCharacteristic(0x2221);
-      }).
+        //return service.getCharacteristic(0x2221);
+      //}).
    // 5.) Zugriff auf die der Charakteristik hinterlegten Daten
-      then(characteristic => {
-      return characteristic.readValue();
-      }).
-      then(value => {
+      //then(characteristic => {
+      //return characteristic.readValue();
+      //}).
+      //then(value => {
     // Zugriff auf Wert
        }).
-    catch(error => {
-    console.error(error);
-});
+    //catch(error => {
+    //console.error(error);
+//});
     
-}  
+//}  
 
 
-function startNotifications(characteristic) {
-  log('Starting notifications...');
+//function startNotifications(characteristic) {
+  //log('Starting notifications...');
 
-  return characteristic.startNotifications().
-      then(() => {
-        log('Notifications started');
-        characteristic.addEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
-      });
-}
+  //return characteristic.startNotifications().
+    //  then(() => {
+      //  log('Notifications started');
+        //characteristic.addEventListener('characteristicvaluechanged',
+          //  handleCharacteristicValueChanged);
+      //});
+//}
 
 // Получение данных
 function handleCharacteristicValueChanged(event) {
