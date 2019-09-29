02f8d0289b1cc48b0ccb0517ce94dec15f389d0c
Ch-Sch <54683753+Ch-Sch@users.noreply.github.com>
2019-09-30 00:38:00 +0200

    Update heartRateSensor.js

--- heart-rate-sensor/heartRateSensor.js
+++ heart-rate-sensor/heartRateSensor.js
@@ -50,30 +50,30 @@
     stopNotificationsHeartRateMeasurement() {
       return this._stopNotifications(0x2221);
     }
-    parseHeartRate(value) {
-      // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
-      value = value.buffer ? value : new DataView(value);
-      let flags = value.getUint8(0);
-      let rate16Bits = flags & 0x1;
-      let result = {};
-      let index = 1;
-      if (rate16Bits) {
-        result.heartRate = value.getUint16(index, /*littleEndian=*/true);
-        index += 2;
-      } else {
-        result.heartRate = value.getUint8(index);
-        index += 1;
-      }
-      let contactDetected = flags & 0x2;
-      let contactSensorPresent = flags & 0x4;
-      if (contactSensorPresent) {
-        result.contactDetected = !!contactDetected;
-      }
-      let energyPresent = flags & 0x8;
-      if (energyPresent) {
-        result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
-        index += 2;
-      }
+ //   parseHeartRate(value) {
+//      // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
+//      value = value.buffer ? value : new DataView(value);
+//      let flags = value.getUint8(0);
+//      let rate16Bits = flags & 0x1;
+//      let result = {};
+//      let index = 1;
+//      if (rate16Bits) {
+//        result.heartRate = value.getUint16(index, /*littleEndian=*/true);
+//        index += 2;
+//      } else {
+//        result.heartRate = value.getUint8(index);
+//        index += 1;
+//      }
+//      let contactDetected = flags & 0x2;
+//      let contactSensorPresent = flags & 0x4;
+//      if (contactSensorPresent) {
+//        result.contactDetected = !!contactDetected;
+//      }
+//      let energyPresent = flags & 0x8;
+//      if (energyPresent) {
+//        result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
+//        index += 2;
+//      } 
     //  let rrIntervalPresent = flags & 0x10;
     //  if (rrIntervalPresent) {
     //    let rrIntervals = [];
@@ -83,7 +83,7 @@
     //    result.rrIntervals = rrIntervals;
      // } 
  
-      return result;
+ //     return result;
     }
 
     /* Utils */
