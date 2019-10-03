4b9164aa22ede9ad596b8714bbe2305fee89885e
Ch-Sch <54683753+Ch-Sch@users.noreply.github.com>
2019-10-03 20:09:58 +0200

    Update heartRateSensor.js

--- heart-rate-sensor/heartRateSensor.js
+++ heart-rate-sensor/heartRateSensor.js
@@ -16,9 +16,9 @@
       .then(server => {
         this.server = server;
         return Promise.all([
-          server.getPrimaryService(0x2220).then(service => {
+          server.getPrimaryService('heart_rate').then(service => {
             return Promise.all([
-             // this._cacheCharacteristic(service, 'body_sensor_location'),
+              //this._cacheCharacteristic(service, 'body_sensor_location'),
               this._cacheCharacteristic(service, 0x2221),
             ])
           })
@@ -52,16 +52,16 @@
     }
     parseHeartRate(value) {
       // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
-        value = value.buffer ? value : new DataView(value);
+      value = value.buffer ? value : new DataView(value);
       let flags = value.getUint8(0);
       let rate16Bits = flags & 0x1;
       let result = {};
       let index = 1;
       if (rate16Bits) {
-        result.heartRate = value.getUint16;(index, /*littleEndian=*/true
+        result.heartRate = value.getUint16(index, /*littleEndian=*/true);
         index += 2;
       } else {
-        result.heartRate = value.getUint8;(index);
+        result.heartRate = value.getUint8(index);
         index += 1;
       }
       let contactDetected = flags & 0x2;
@@ -71,18 +71,17 @@
       }
       let energyPresent = flags & 0x8;
       if (energyPresent) {
-        result.energyExpended = value.getUint16(index, /*littleEndian=*/true
+        result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
         index += 2;
-      } 
+      }
       let rrIntervalPresent = flags & 0x10;
       if (rrIntervalPresent) {
         let rrIntervals = [];
         for (; index + 1 < value.byteLength; index += 2) {
-          rrIntervals.push(value.getUint16(index, /*littleEndian=*/true
+          rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
         }
         result.rrIntervals = rrIntervals;
-     } 
- 
+      }
       return result;
     }
 
@@ -109,15 +108,15 @@
     }
     _startNotifications(characteristicUuid) {
       let characteristic = this._characteristics.get(characteristicUuid);
-       Returns characteristic to set up characteristicvaluechanged event
-       handlers in the resolved promise.
+      // Returns characteristic to set up characteristicvaluechanged event
+      // handlers in the resolved promise.
       return characteristic.startNotifications()
       .then(() => characteristic);
     }
     _stopNotifications(characteristicUuid) {
       let characteristic = this._characteristics.get(characteristicUuid);
-       Returns characteristic to remove characteristicvaluechanged event
-       handlers in the resolved promise.
+      // Returns characteristic to remove characteristicvaluechanged event
+      // handlers in the resolved promise.
       return characteristic.stopNotifications()
       .then(() => characteristic);
     }
