a213ed6ddab8b328dc49a6399e8aa903728a1748
Ch-Sch <54683753+Ch-Sch@users.noreply.github.com>
2019-10-04 03:24:00 +0200

    Update heartRateSensor.js

--- heart-rate-sensor/heartRateSensor.js
+++ heart-rate-sensor/heartRateSensor.js
@@ -64,26 +64,26 @@
         result.heartRate = value.getUint8(index);
         index += 1;
       }
-      /*let contactDetected = flags & 0x2;
-      let contactSensorPresent = flags & 0x4;
-      if (contactSensorPresent) {
-        result.contactDetected = !!contactDetected;
-      }
-      let energyPresent = flags & 0x8;
-      if (energyPresent) {
-        result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
-        index += 2;
-      }
-      let rrIntervalPresent = flags & 0x10;
-      if (rrIntervalPresent) {
-        let rrIntervals = [];
-        for (; index + 1 < value.byteLength; index += 2) {
-          rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
-        }
-        result.rrIntervals = rrIntervals;*/
-      }
-      return result;
-    }
+     // let contactDetected = flags & 0x2;
+     // let contactSensorPresent = flags & 0x4;
+     // if (contactSensorPresent) {
+     //   result.contactDetected = !!contactDetected;
+     // }
+     // let energyPresent = flags & 0x8;
+     // if (energyPresent) {
+     //   result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
+     //   index += 2;
+     // }
+     // let rrIntervalPresent = flags & 0x10;
+     // if (rrIntervalPresent) {
+     //   let rrIntervals = [];
+     //   for (; index + 1 < value.byteLength; index += 2) {
+     //     rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
+     //   }
+     //   result.rrIntervals = rrIntervals;
+     // }
+     // return result;
+    //}
 
     /* Utils */
 
